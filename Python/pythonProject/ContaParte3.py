2d47fc7fe43c6afd05f057b11bed474993ee2f82
gabriel.pilon <gabriel.correia@globalhitss.com.br>
2021-02-08 18:05:35 -0300

    Continuação do projeto (Conta) de orientação objeto

--- Python/pythonProject/ContaParte3.py
+++ Python/pythonProject/ContaParte3.py
@@ -6,16 +6,20 @@ class Conta:
                                                         ##Self é a referência do objeto
                                                         ##O Ponto você acessa o objeto
         print("Construindo objeto... {}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero         ## com os dois underlines (__) eu torno o atributo privado -  no Java seria (private numero)
+        self.__titular = titular       ## com os dois underlines (__) eu torno o atributo privado
+        self.__saldo = saldo           ## com os dois underlines (__) eu torno o atributo privado
+        self.__limite = limite         ## com os dois underlines (__) eu torno o atributo privado
 
     def extrato(self):
-        print("O saldo do titular {} é de {} reais".format(self.titular,self.saldo))
+        print("O saldo do titular {} é de {} reais".format(self.__titular,self.__saldo))
 
     def depositar(self,valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def sacar(self,valor):
-        self.saldo -= valor
+        self.__saldo -= valor
+
+    def transfere(self,valor,destino): ## EXEMPLO DE ENCAPSULAMENTO
+        self.sacar(valor)
+        destino.depositar(valor)
